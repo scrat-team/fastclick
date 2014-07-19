6dadabc75ba51a7f38ef273dbb2abd9fe4a515b6
Hinc Liu <hincubus@gmail.com>
2014-07-19 18:26:58 +0800

    初始化版本库

---
+++ README.md
@@ -0,0 +1,4 @@
+fastclick
+=========
+
+transform [ftlabs/fastclick](https://github.com/ftlabs/fastclick) into installable [scrat-component](https://github.com/scrat-team) with [Travis](https://travis-ci.org/)
---
+++ config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  orgiRepo: 'ftlabs/fastclick',
+  ref: 'master',
+  target: __dirname,
+  name: 'fastclick',
+  repo: 'scrat-team/fastclick',
+  description: 'a scrat-installable fork of ftlabs/fastclick',
+  main: {
+    'fastclick.js': 'lib/fastclick.js'
+  }
+};
